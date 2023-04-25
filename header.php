03ee61d15cbba2f62a3666c50b8966f3ad48ca47
Rudranil Chakraborty <99527741+revampCode@users.noreply.github.com>
2023-04-25 20:53:31 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta.charset="UTF-8">
+        <meta name="viewport" content="width= device-width, initial-scale=1.0">
+        <meta http-eequiv="X-UA-Compatible" content="ie=edge">
+        <title> Real time chat app</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/0f1eb19a54.js" crossorigin="anonymous"></script>
+    </head>
